598bb7f85d3e890c7fcb37c69ca9bb4abf5631b8
Henrique Couto <henrique.coutu@live.com>
2020-02-15 18:23:16 -0300

    basic router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Typography } from "@material-ui/core";
+import { Home as HomeIcon } from "@material-ui/icons";
+import Header from "./components/Header";
+
+const routes = [
+  {
+    render: <Typography variant="h1">Início</Typography>,
+    path: "/",
+    name: "Início",
+    icon: () => <HomeIcon />
+  },
+  {
+    render: <Typography variant="h1">Área do Pesquisador</Typography>,
+    path: "/area-do-pesquisador",
+    name: "Área do Pesquisador",
+    icon: () => <HomeIcon />
+  },
+  {
+    render: <Typography variant="h1">PIBIC</Typography>,
+    path: "/pibic",
+    name: "PIBIC",
+    icon: () => <HomeIcon />
+  },
+  {
+    render: <Typography variant="h1">Grupod de Pesquisa</Typography>,
+    path: "/grupos-de-pesquisa",
+    name: "Grupos de Pesquisa",
+    icon: () => <HomeIcon />
+  },
+  {
+    render: <Typography variant="h1">Pós Graduação</Typography>,
+    path: "/pos-graduacao",
+    name: "Pós Graduação",
+    icon: () => <HomeIcon />
+  },
+  {
+    render: <Typography variant="h1">Coordenação</Typography>,
+    path: "/coordenacao",
+    name: "Coordenação",
+    icon: () => <HomeIcon />
+  }
+];
+
 function App() {
-  return <Typography variant="h1">Hello</Typography>;
+  const [position, setPosition] = useState("/");
+  return (
+    <Router>
+      <Header position={position} routes={routes}>
+        {routes.map(v => {
+          return (
+            <Route exact path={v.path} key={v.path}>
+              {v.render}
+            </Route>
+          );
+        })}
+      </Header>
+    </Router>
+  );
 }
 
 export default App;
