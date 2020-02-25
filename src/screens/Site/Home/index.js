290b8950561a7f120e2ae678e7fc6ea3c7fd10e6
Henrique Couto <henrique.coutu@live.com>
2020-02-25 05:25:59 -0300

    showing activities

--- src/screens/Site/Home/index.js
+++ src/screens/Site/Home/index.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Grid } from "@material-ui/core";
+import { Typography, Grid, Icon } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { getFile } from "../../../services/storage";
 import CustomCard from "../../../components/CustomCard";
+import {
+  loadCoordinationActivities,
+  loadCoordination
+} from "../../../services/db";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -17,15 +21,38 @@ const useStyles = makeStyles(theme => ({
     maxWidth: 1200,
     padding: theme.spacing(2, 2, 2, 2)
   },
-  activities: {
-    maxWidth: 400,
-    marginTop: theme.spacing(2)
+  activity: {
+    maxWidth: 600
+  },
+  icon: {
+    height: 80,
+    width: 80,
+    fontSize: 35,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(2),
+    color: "white",
+    borderRadius: 80
   }
 }));
 
 export default function Home({ setPosition }) {
   const classes = useStyles();
   const [logo, setLogo] = useState("");
+  const [coordActivities, setCoordActivities] = useState([]);
+  const [coordination, setCoordination] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = loadCoordinationActivities(setCoordActivities);
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = loadCoordination(setCoordination);
+    return () => unsubscribe();
+  }, []);
 
   const handleImage = async () => {
     setLogo(await getFile("assets/logo-ufal.png"));
@@ -53,90 +80,48 @@ export default function Home({ setPosition }) {
 
       <Grid item container className={classes.item}>
         <CustomCard variant="light">
-          <Typography variant="body1">
-            A{" "}
-            <strong>
-              Coordenação de Pesquisa da Unidade Educacional de Penedo
-            </strong>{" "}
-            é responsável por articular a pesquisa às atividades de ensino e de
-            extensão; prospectar as possibilidades de parcerias para pesquisa e
-            inovação; fomentar a realização de eventos de pesquisa e fortalecer
-            e acompanhar os grupos de pesquisa certificados pelo CNPQ.
-          </Typography>
-          <Typography variant="body1">
-            Além disso, a coordenação de pesquisa integra o comitê assessor da
-            Pró-Reitoria de Pesquisa da UFAL e tem como principais atribuições
-            locais as seguintes atividades:
-          </Typography>
-          <Grid container spacing={2} justify="center">
-            <Grid item container className={classes.activities} spacing={2}>
-              <Grid item>
-                <Typography>
-                  Apreciar e dar parecer aos projetos de pesquisa apresentados
-                  pelos docentes da Unidade;
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography>
-                  Encaminhar para o avaliadores externos a análise do mérito dos
-                  projetos de pesquisa da Unidade que visam à concessão de
-                  bolsas de iniciação científica, tecnológica e financiamentos
-                  em geral;
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography>
-                  Acompanhar e avaliar projetos de pesquisa em execução e
-                  mediante análise de relatórios da produção científica,
-                  científica, tecnológica, artística e cultural gerada, dando
-                  parecer circunstanciado;
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography>
-                  Formar Comissão Interna para auxiliar no processo das
-                  atividades de pesquisa da unidade acadêmica;
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography>
-                  Constituir e manter atualizado o quadro de consultores “ad
-                  hoc” a fim de auxiliar as avaliações externas dos Projetos;
-                </Typography>
-              </Grid>
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <Typography variant="body1">
+                A{" "}
+                <strong>
+                  Coordenação de Pesquisa da Unidade Educacional de Penedo
+                </strong>{" "}
+                é responsável por articular a pesquisa às atividades de ensino e
+                de extensão; prospectar as possibilidades de parcerias para
+                pesquisa e inovação; fomentar a realização de eventos de
+                pesquisa e fortalecer e acompanhar os grupos de pesquisa
+                certificados pelo CNPQ.
+              </Typography>
             </Grid>
-            <Grid item container className={classes.activities} spacing={2}>
-              <Grid item>
-                <Typography>
-                  Desenvolver estudos e análises e promover debates que permitam
-                  fornecer subsídios para a fixação, aperfeiçoamento e
-                  modificação da política de pesquisa da unidade acadêmica;
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography>
-                  Manifestar-se sobre qualquer assunto relativo às atividades de
-                  pesquisa da unidade acadêmica, quando solicitado;
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography>
-                  Manifestar-se sobre os aspectos legais dos projetos da unidade
-                  acadêmica que representa;
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography>
-                  Criar e manter atualizado um banco de dados sobre as pesquisas
-                  da unidade acadêmica que representa;
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography>
-                  Zelar pelos cumprimentos das Normas e Editais vigentes
-                  relativos ao Pibic e Pibiti.
-                </Typography>
-              </Grid>
+            <Grid item>
+              <Typography variant="body1">
+                Além disso, a coordenação de pesquisa integra o comitê assessor
+                da Pró-Reitoria de Pesquisa da UFAL e tem como principais
+                atribuições locais as seguintes atividades:
+              </Typography>
+            </Grid>
+            <Grid item container spacing={10} justify="center">
+              {coordActivities.map(v => {
+                return (
+                  <Grid
+                    item
+                    container
+                    direction="row"
+                    alignItems="center"
+                    className={classes.activity}
+                    spacing={2}
+                    key={v.id}
+                  >
+                    <Grid item>
+                      <Icon className={classes.icon}>{v.icon}</Icon>
+                    </Grid>
+                    <Grid item xs>
+                      <Typography>{v.name}</Typography>
+                    </Grid>
+                  </Grid>
+                );
+              })}
             </Grid>
           </Grid>
         </CustomCard>
